e9fc4af29083a1c6299572cf780733edd62d9be4
qFurtive <ryalmeevv@gmail.com>
2024-11-07 00:32:30 +0400

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,26 @@
+calls = 0
+
+def count_calls():
+
+    global calls
+    calls += 1
+def string_info(string):
+
+    count_calls()
+    return {len(string), string.lower(), string.upper()}
+
+def is_contains(string, list_to_search):
+
+    count_calls()
+    if string.lower() in (item.lower() for item in list_to_search):
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
+
